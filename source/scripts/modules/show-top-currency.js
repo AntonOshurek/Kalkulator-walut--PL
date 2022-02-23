daa7336faff41bbf32ee4260ce8c52e56e9f48ce
Anton Ashurek <ANAS@aconto.unicorn.com.pl>
2022-02-23 14:24:55 +0100

    show geting data about currency

--- source/scripts/modules/show-top-currency.js
+++ source/scripts/modules/show-top-currency.js
@@ -1,27 +1,33 @@
 import { getOneCurrency } from '../api';
 
+const allCurrencyItems = document.querySelectorAll('.top-five__item');
+
 const transformCurrencyData = (obj) => {
   const buy = obj.rates[0].bid;
   const sell = obj.rates[0].ask;
-  return {buy, sell};
-}
-
+  const code = obj.code;
+  return {buy, sell, code};
+};
 
 const topCurrency = () => {
 
-  getOneCurrency('USD').then((data) => {
-    showTopCurrency(data);
-  }).catch((err) => {
-    console.error(`błąd pobierania danych - ${err}`);
+  allCurrencyItems.forEach((item) => {
+    const currencyName = item.getAttribute('data-currency-name');
+
+    getOneCurrency(currencyName).then((data) => {
+      showTopCurrency(data, item);
+    }).catch((err) => {
+      console.error(`błąd pobierania danych - ${err}`);
+    });
   });
 
-  const showTopCurrency = (data) => {
+  const showTopCurrency = (data, item) => {
+    const {sell, buy, code} = transformCurrencyData(data)
+    item.querySelector('.top-five__price--sell').textContent = sell;
+    item.querySelector('.top-five__price--buy').textContent = buy;
+    item.querySelector('.top-five__currency-name').textContent = code;
+  };
 
-  console.log(transformCurrencyData(data).buy);
-  console.log(transformCurrencyData(data).sell);
-  }
-
-
-}
+};
 
 export { topCurrency };
