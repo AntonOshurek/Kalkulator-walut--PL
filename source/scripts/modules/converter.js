123551a751de5dc9b4976c6acf58b4fa1ec31229
Anton Ashurek <ANAS@aconto.unicorn.com.pl>
2022-02-24 15:13:06 +0100

    add basic convert logick

--- source/scripts/modules/converter.js
+++ source/scripts/modules/converter.js
@@ -1,6 +1,6 @@
 import { getAllCurrency } from '../api';
 
-const allSelects = document.querySelectorAll('.converter__select');
+const allConvertItems = document.querySelectorAll('.converter__item');
 const optionTemplate = document.querySelector('#converter__option');
 
 const transformCurrencyData = (obj) => {
@@ -17,21 +17,52 @@ const converter = () => {
   });
 
   const showCurrencyItems = (data) => {
-    allSelects.forEach((item) => {
+    allConvertItems.forEach((item, i) => {
 
       const fragment = new DocumentFragment();
 
       for (let key in data) {
-        console.log(data[key])
         const templateItem = optionTemplate.content.cloneNode(true);
         templateItem.querySelector('.converter__option').value = data[key].code;
         templateItem.querySelector('.converter__option').textContent = data[key].currency;
 
         fragment.append(templateItem);
       }
-      item.append(fragment);
+
+      //setting id attributes for input and select
+      item.querySelector('.converter__input').setAttribute('currency-id', `cur${i}`);
+      item.querySelector('.converter__select').setAttribute('currency-id', `cur${i}`);
+
+      item.querySelector('.converter__select').append(fragment);
+
+      checkConvertingValue(item)
     })
   };
+
+  let inputValue = '';
+
+  const checkConvertingValue = (item) => {
+
+    item.querySelector('.converter__input').addEventListener('input', (evt) => {
+      inputValue = evt.target.value;
+      setValue();
+    });
+
+    const setValue = () => {
+
+      allConvertItems.forEach((convertItem) => {
+        if(convertItem === item) {
+          convertItem.querySelector('.converter__input').value = +inputValue;
+        } else {
+          convertItem.querySelector('.converter__input').value = +inputValue * 2;
+          console.log(convertItem.querySelector('.converter__select').value);
+
+        }
+      });
+
+    }
+
+  }
 
 };
 
