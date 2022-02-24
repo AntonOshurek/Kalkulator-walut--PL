3ff415b59c64cb4386d5e24adfc9e5182e2605e9
Anton Ashurek <ANAS@aconto.unicorn.com.pl>
2022-02-24 13:26:01 +0100

    show all valutes in options

--- source/scripts/api.js
+++ source/scripts/api.js
@@ -2,8 +2,13 @@ const GET_ONE_CURRENCY_SOURCE = 'http://api.nbp.pl/api/exchangerates/rates/';
 const GET_ALL_CURRENCY_SOURCE = 'http://api.nbp.pl/api/exchangerates/tables/';
 const BASI_TABLE_CODE = 'c';
 
+// API souerce - http://api.nbp.pl/
+
 // {table} – typ tabeli (A, B, lub C)
 // {code} – trzyliterowy kod waluty (standard ISO 4217)
+
+// format JSON: nagłówek Accept: application/json lub parameter ?format=json
+// format XML: nagłówek Accept: application/xml lub parameter ?format=xml
 
 const getOneCurrency = (code, table = BASI_TABLE_CODE) => fetch(`${GET_ONE_CURRENCY_SOURCE}${table}/${code}/?format=json`, {
     Accept: 'application/json'
--- source/scripts/modules/converter.js
+++ source/scripts/modules/converter.js
@@ -1,12 +1,37 @@
 import { getAllCurrency } from '../api';
 
+const allSelects = document.querySelectorAll('.converter__select');
+const optionTemplate = document.querySelector('#converter__option');
+
+const transformCurrencyData = (obj) => {
+  const curItems = obj[0].rates
+  return {...curItems};
+};
+
 const converter = () => {
 
   getAllCurrency().then((data) => {
-    console.log(data)
+    showCurrencyItems(transformCurrencyData(data));
   }).catch((err) => {
     console.error(err);
   });
+
+  const showCurrencyItems = (data) => {
+    allSelects.forEach((item) => {
+
+      const fragment = new DocumentFragment();
+
+      for (let key in data) {
+        console.log(data[key])
+        const templateItem = optionTemplate.content.cloneNode(true);
+        templateItem.querySelector('.converter__option').value = data[key].code;
+        templateItem.querySelector('.converter__option').textContent = data[key].currency;
+
+        fragment.append(templateItem);
+      }
+      item.append(fragment);
+    })
+  };
 
 };
 
